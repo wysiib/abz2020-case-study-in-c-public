6ccfaf080f7cccd82183531447b104cbdbc5633f
Sebastian Krings <sebastian@krin.gs>
2019-11-20 11:27:35 +0100

    Fix scenario 7

--- light/light-impl.h
+++ light/light-impl.h
@@ -1,7 +1,7 @@
 #ifndef LIGHT_LIGHT_IMPL_H_INCLUDED
 #define LIGHT_LIGHT_IMPL_H_INCLUDED
 
-typedef enum {blink_left, blink_right, hazard, none} blinkingDirection;
+typedef enum {blink_left, blink_right, tip_left, tip_right, hazard, none} blinkingDirection;
 
 
 void reset(void **state);
